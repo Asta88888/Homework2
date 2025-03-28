054c71e1b44aeea1f5ef4cd0387963664f5bb5e1
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-03-28 19:18:27 +0300

    update classes product and category

--- src/category.py
+++ src/category.py
@@ -12,23 +12,37 @@ class Category:
         """Конструктор класса Category, для создания экземпляров класса"""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
 
 
-product1 = Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    def add_product(self, product):
+        """Функция добавляет новый продукт в список продуктов"""
+        self.__products.append(product)
+        Category.product_count += 1
 
-category1 = Category(
-    "Смартфоны",
-    "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
-    [product1, product2, product3],
-)
-category2 = Category(
-    "Телевизоры",
-    "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-    [product4],
-)
+
+    @property
+    def products(self):
+        """Геттер для вывода списка продуктов в виде строки"""
+        return "\n".join(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт." for product in self.__products)
+
+
+# product1 = Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+# product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+# product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+# product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+#
+# category1 = Category(
+#     "Смартфоны",
+#     "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
+#     [product1, product2, product3],
+# )
+# category2 = Category(
+#     "Телевизоры",
+#     "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+#     [product4],
+# )
+# print(category1.products)
+# print(category2.products)
--- src/product.py
+++ src/product.py
@@ -1,12 +1,66 @@
+
+
+
 class Product:
     name: str
     description: str
     price: float
     quantity: int
+    product_list = []
+
 
     def __init__(self, name, description, price, quantity):
         """Конструктор класса Product, для создания нового экземпляра"""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+
+    @classmethod
+    def new_product(cls, new_product):
+        """Добавление нового продукта"""
+        for product in cls.product_list:
+            if product.name == new_product["name"]:
+                product.quantity += new_product["quantity"]
+                product.price = max(product.price, new_product["price"])
+                return product
+        new_item = cls(
+            new_product["name"],
+            new_product["description"],
+            new_product["price"],
+            new_product["quantity"]
+        )
+        cls.product_list.append(new_item)
+        return new_item
+
+    @property
+    def price(self):
+        """Геттер для цены"""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        """Сеттер для цены с проверкой на отрицательные суммы и на уменьшение цены"""
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        if new_price < self.__price:
+            print(f"Понизить цену с {self.__price} до {new_price} или оставить прежней? yes/no")
+            user_answer = input().lower()
+            if user_answer == "yes":
+                self.__price = new_price
+                print(f"Цена изменена. Старая цена: {self.__price}, новая цена: {new_price}")
+            else:
+                print("Цена не изменена")
+        else:
+            self.__price = new_price
+            print(f"Цена {new_price}")
+
+
+
+
+# new_product = Product.new_product(
+#         {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+#          "quantity": 5})
+# print(new_product.__dict__)
